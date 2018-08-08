a20e53dd3979b10291f83ff6b7c33ba901397503
Mario Silva <marioposilva@gmail.com>
2018-08-08 17:42:39 +0100

    FEAT : policyCancel

---
+++ policyCancel/gulp/clean.js
@@ -0,0 +1,17 @@
+// clean.js
+const gulp = require('gulp'),
+  cfg = require('./_config'),
+  plumber = require('gulp-plumber'),
+  gutil = require('gulp-util'),
+  clean = require('gulp-clean');
+
+gulp.task('clean', () => {
+	var globs = './dist';
+	return gulp.src(globs, {read: false})
+		.pipe(plumber(function (error) {
+			gutil.log(error.message);
+			this.emit('end');
+		}))
+		.pipe(clean({force: true}))
+		.pipe(clean());
+});
